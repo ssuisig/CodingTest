ec70986d0e1de828d98754fb2466e47a906ec133
suyeon <109880402+ssuisig@users.noreply.github.com>
2024-11-26 01:31:06 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.14 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -3,42 +3,31 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
-        int index = -1;
+        Set<String> set = new HashSet<>(); // 중복 확인용
+        set.add(words[0]); // 첫 단어는 무조건 추가
         
-        List<String> list = Arrays.asList(words);
-        Set<String> set = new HashSet<>();
-        
-        set.add(words[0]);
-        for(int i=1; i < words.length; i++){
-            String s1 = words[i-1];
+        for (int i = 1; i < words.length; i++) {
+            String s1 = words[i - 1];
             String s2 = words[i];
-            if(s1.charAt(s1.length()-1)!=s2.charAt(0)){
-                System.out.println("noflow");
-                index = i;
-                break;
+
+            // 규칙 1: 이어지는 단어가 올바르지 않을 경우
+            if (s1.charAt(s1.length() - 1) != s2.charAt(0)) {
+                answer[0] = (i % n) + 1; // 플레이어 번호
+                answer[1] = (i / n) + 1; // 차례
+                return answer;
             }
-            if(!set.add(words[i])){
-                System.out.println("duplication");
-                index = i;
-                break;
+
+            // 규칙 2: 단어 중복 확인
+            if (set.contains(s2)) {
+                answer[0] = (i % n) + 1; // 플레이어 번호
+                answer[1] = (i / n) + 1; // 차례
+                return answer;
             }
-            
-        }
-        System.out.println(index);
-        if(index != -1){
-            index += 1;
-            System.out.println(index);
-            if(index % n == 0){
-                answer[0] = n;
-                answer[1] = index/n;
-            }
-            else{
-                answer[0] = index%n;
-                answer[1] = index/n+1;
-            }
-            
+
+            set.add(s2); // 단어 사용 처리
         }
 
-        return answer;
+        // 규칙 위반이 없을 경우
+        return answer; // [0, 0] 반환
     }
 }
